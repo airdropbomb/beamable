7b781d63c851f87d554b7df357fb036bbb803654
airdropbomb <saginaw135@gmail.com>
2025-03-23 16:26:56 +0630

    Update task.js

--- task.js
+++ task.js
@@ -26,7 +26,7 @@ async function readToken() {
 }
 
 // Function to fetch unclaimed quests using Puppeteer
-const browser = await puppeteer.launch({
+  const browser = await puppeteer.launch({
   headless: true,
   executablePath: '/usr/bin/chromium-browser', // Contabo VPS မှာ Chromium ရဲ့ path
   args: browserArgs,
